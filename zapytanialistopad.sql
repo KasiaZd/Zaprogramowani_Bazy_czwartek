0a59daa0dabdbe987a5cbb57f11e1558af68aed5
KasiaZd <kasiadzb@onet.eu>
2020-11-26 19:15:47 +0100

    Add files via upload

--- zapytanialistopad.sql
+++ zapytanialistopad.sql
@@ -131,4 +131,151 @@ select pi() as PI,
 	   select id, nazwa, sku from PRODUKTY p  
 	   where typ_produktu  in ('owoce') 
 	   order by nazwa asc
+	   
+	   
+	   select id, nazwa, sku from PRODUKTY p  
+	   where typ_produktu  in ('owoce') 
+	   order by sku desc
+	   
+	   select id, nazwa, sku from PRODUKTY p  
+	   where typ_produktu  in ('owoce') 
+	   order by sku 
 
+	   
+select id, pozycja from SLOWNIK_POZYCJI sp  
+order by pozycja 
+
+select id, pozycja from SLOWNIK_POZYCJI sp  
+order by id asc
+
+select id, pozycja from SLOWNIK_POZYCJI sp  
+order by pozycja desc
+
+
+select * from produkty
+
+select avg(cena_jednostkowa) as cena, 
+	   typ_produktu
+	   from PRODUKTY p 
+group by typ_produktu 	   
+
+
+select round(avg(cena_jednostkowa),2) as cena, 
+	   typ_produktu
+	   from PRODUKTY p 
+group by typ_produktu 	   
+
+
+
+select * from zamowienia
+
+select * from ZAMOW_PRODUKT zp 
+
+select * from PLATNOSCI p 
+
+select * from PRODUKTY p 
+
+
+CREATE VIEW produkty_v AS
+SELECT id, nazwa, sku, CENA_JEDNOSTKOWA 
+FROM produkty
+WHERE typ_produktu in ('owoce', 'warzywa');
+
+
+insert into PRODUKTY  (id, NAZWA, sku, CENA_JEDNOSTKOWA, typ_produktu) values (8, 'marchew', 15, 2.30, 'warzywa'),
+																	 (9, 'pietruszka', 16, 7, 'warzywa')
+					
+select * from adresy 
+select * from UZYTKOWNIK u 
+select * from SLOWNIK_POZYCJI sp 
+																	 
+																	 
+select * from produkty_v pv 
+
+
+create view adresy_uzytkown as
+select a.imie, 
+	   a.nazwisko, 
+	   a.ulica, 
+	   a.miasto, 
+	   a.kod_pocztowy, 
+	   a.numer_domu, 
+	   a.numer_lokalu, 
+	   u.nazwa,
+	   sp.pozycja
+	   
+	   from ADRESY a  
+	   join UZYTKOWNIK u on u.id = a.uzytkownik_id 
+	   join SLOWNIK_POZYCJI sp on u.SLOWNIK_POZYCJI_ID  = sp.id
+
+
+
+	   select * from adresy_uzytkown 
+
+
+select getdate()
+
+
+
+select datepart(wk, getdate()) as dzi_tyg,
+	   datepart(wk, '01-01-2020') as dz_tyg,
+	   datepart(ww, '01-01-2020') as dzie_tyg,
+	   datepart(year, getdate()) as rok,
+	   datepart(yyyy, getdate()) as rok2,
+	   datepart(yy,getdate()) as rok3,
+	   datepart(mm, getdate()) as miesiac,
+	   datepart(month, getdate()) as miesiac2,
+	   datepart(m, getdate()) as miesiac3,
+	   datepart(quarter, getdate()) as kwartal,
+	   datepart(qq, getdate()) as kwartal2,
+	   datepart(q, getdate()) as kwartal3,
+       datepart(dayofyear, getdate()) as dzien_roku,
+       DATEPART(DAYOFYEAR, getdate()+28) as wigilia
+
+       
+select datepart(DAYOFYEAR, '2020-12-24') as wig,
+	   datepart(dy, getdate()) as dzien_roku, 
+	   datepart(y, getdate()) as dzien_roku2,
+	   datepart(day, getdate()) as dzien_miesiaca, 
+	   datepart(dd, getdate()) as dzien_miesiaca2, 
+	   datepart(d, getdate()) as dzien_miesiaca3,
+	   datepart(week, getdate()) as tydzien_roku, 
+	   datepart(weekday, getdate()) as dzien_tygodnia,
+	   datepart(dw, getdate()) as dzien_tygodnia2,
+	   datepart(hour, getdate()) as godzina, 
+	   datepart(hh, getdate()) as godzina2,
+	   datepart(MINUTE, getdate()) as minuta, 
+	   DATEPART(mi, getdate()) as minuta2,
+	   datepart(n, getdate()) as minuta3
+	   
+	   
+select DATEPART(ss, getdate()) as sekundy,
+	   datepart(s, getdate()) as sekundy2,
+	   datepart(SECOND, getdate()) as sekundy3,
+	   datepart(millisecond, getdate()) as milisekundy,
+	   datepart(ms, getdate()) as milisekundy2,
+	   datepart(microsecond, getdate()) as mikrosekundy, 
+	   datepart(mcs, getdate()) as mikrosekundy2, 
+	   datepart(nanosecond, getdate()) as nanosekundy, 
+	   datepart(ns, getdate()) as nanosekundy2,
+
+	   --datepart(tz, GETDATE()) as timezone2,
+	   DATEPART(iso_week, getdate()) as isoweek  
+	   
+	   
+select day(getdate()),
+	   month(getdate()),
+	   year(getdate()),
+	   datename(yyyy,getdate()),
+	   datename(mm, getdate()),
+	   datename(dd, getdate()),
+	   datename(weekday, getdate()),
+	   datename(dw, getdate())
+	   
+	   
+
+
+
+
+
+	   
